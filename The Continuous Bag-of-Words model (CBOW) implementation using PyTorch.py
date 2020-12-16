6317e287d19c15431d4a02e8ec8c7f54b7d157f6
adurukan <alpaysedatdurukan@gmail.com>
2020-12-16 02:23:45 +0100

    Fixed minor issues

--- The Continuous Bag-of-Words model (CBOW) implementation using PyTorch.py
+++ The Continuous Bag-of-Words model (CBOW) implementation using PyTorch.py
@@ -4,13 +4,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 torch.manual_seed(1)
-'''
-The Continuous Bag-of-Words model (CBOW) is frequently used in NLP deep learning. It is a model that tries to predict words given
-the context of a few words before and a few words after the target word. This is distinct from language modeling, since CBOW is not
-sequential and does not have to be probabilistic. Typically, CBOW is used to quickly train word embeddings, and these embeddings are
-used to initialize the embeddings of some more complicated model. Usually, this is referred to as pretraining embeddings. It almost
-always helps performance a couple of percent.
-'''
 
 CONTEXT_SIZE = 2  # 2 words to the left, 2 to the right
 raw_text = """We are about to study the idea of a computational process.
@@ -22,7 +15,6 @@ we conjure the spirits of the computer with our spells.""".split()
 # By deriving a set from `raw_text`, we deduplicate the array
 vocab = set(raw_text)
 vocab_size = len(vocab)
-# print(vocab_size)
 embedding_dim = 10
 
 word_to_ix = {word: i for i, word in enumerate(vocab)}
@@ -33,8 +25,6 @@ for i in range(2, len(raw_text) - 2):
     target = raw_text[i]
     data.append((context, target))
 
-
-# print(data[:5])
 
 class CBOW(nn.Module):
     def __init__(self, vocab_size, embedding_dim, CONTEXT_SIZE):
@@ -64,8 +54,6 @@ optimizer = optim.SGD(model.parameters(), lr=0.001)
 for epoch in range(10):
     total_loss = 0
     for context, target in data:
-        # Step 1. Prepare the inputs to be passed to the model (i.e, turn the words
-        # into integer indices and wrap them in tensors)
         context_idxs = torch.tensor([word_to_ix[w] for w in context], dtype=torch.long)
         model.zero_grad()
         log_probs = model(context_idxs)
@@ -74,6 +62,5 @@ for epoch in range(10):
         optimizer.step()
         total_loss = total_loss + loss.item()
     losses.append(total_loss)
-print(data[:5])
 print(losses)
 
